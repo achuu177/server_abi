905ff0c8e7f3e93bb86ae33f362f9baf2bd36eb0
achuu177 <aswathikp120@gmail.com>
2025-02-12 21:02:31 +0530

    fourth commit

--- index.js
+++ index.js
@@ -17,7 +17,7 @@ app.use(express.json());
 
 app.use(
   cors({
-      origin: ["http://localhost:5173", "https://client-abi.vercel.app/"],
+      origin: ["http://localhost:5173", "https://client-abi.vercel.app"],
       methods: ["GET", "PUT", "POST", "DELETE", "OPTIONS"],
       credentials: true,
   })
